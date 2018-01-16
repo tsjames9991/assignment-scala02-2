95210f19f8b39e408d90871e1cb78e8944cef6f7
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-16 17:43:08 +0530

    First Draft

---
+++ src/main/scala/edu/knoldus/auxiliary/BillGenerator.scala
@@ -0,0 +1,33 @@
+package edu.knoldus.auxiliary
+
+import org.apache.log4j.Logger
+
+class BillGenerator {
+
+  abstract class TiffinService(payment: Double = 0)
+
+  case class Paytm(payment: Double) extends TiffinService(payment)
+
+  case class NetBanking(payment: Double) extends TiffinService(payment)
+
+  case class CardPayment(payment: Double) extends TiffinService(payment)
+
+  case class Cash(payment: Double) extends TiffinService(payment)
+
+  def printBill(tf: TiffinService): Double = {
+    tf match {
+      case Paytm(payment: Double) => payment + ((payment * 2)/100)
+      case NetBanking(payment: Double) => payment + 5
+      case CardPayment(payment: Double) => payment + 1.5
+      case Cash(payment: Double) => payment
+    }
+  }
+}
+
+object BillGenerator extends App {
+  val obj = new BillGenerator
+  val billAmount: Double = 100
+  val log = Logger.getLogger(this.getClass)
+  log.info("\nUsing PayTM\n")
+  log.info(s"${obj.printBill(obj.Paytm(billAmount))}")
+}
---
+++ src/main/scala/edu/knoldus/auxiliary/Person.scala
@@ -0,0 +1,5 @@
+package edu.knoldus.auxiliary
+
+abstract class Person {
+
+}
